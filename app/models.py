c674e1ecf529bbe0450088ae54550a5ca91598e0
arcturus5340 <arcturus5340@gmail.com>
2019-08-10 23:57:43 +0500

    added link block and url access

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 import django.db.models
 
+
 class Files(django.db.models.Model):
     location = django.db.models.TextField()
     link = django.db.models.TextField()
-    blocked = django.db.models.IntegerField()
+    blocked = django.db.models.IntegerField(default=0)
+    url_access = django.db.models.IntegerField(default=0)
+    allowed_urls = django.db.models.TextField(default='')
